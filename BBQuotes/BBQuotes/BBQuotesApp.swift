b84cfb70fe161b8ce3ad7044f933832485ad2470
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-12-20 14:12:08 +0000

    Proj setup and initial tabview implementation
    Also added assets

---
+++ BBQuotes/BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by José João Pimenta Oliveira on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBQuotes/BBQuotes/Views/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  BBQuotes
+//
+//  Created by José João Pimenta Oliveira on 20/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("Breaking Bad View")
+                .tabItem { Label("Breaking Bad", systemImage: "tortoise") }
+
+            Text("Better Call Saul View")
+                    .tabItem { Label("Better Call Saul", systemImage: "briefcase") }
+        }
+        .onAppear {
+            UITabBar.appearance().scrollEdgeAppearance = UITabBarAppearance()
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
