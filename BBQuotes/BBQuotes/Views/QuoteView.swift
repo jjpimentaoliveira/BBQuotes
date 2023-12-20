c53bd9b5dacbe95acd8682e7240361d1e3d7074d
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-12-20 16:52:33 +0000

    implement fetch status to quote view

--- BBQuotes/BBQuotes/Views/QuoteView.swift
+++ BBQuotes/BBQuotes/Views/QuoteView.swift
@@ -8,45 +8,73 @@
 import SwiftUI
 
 struct QuoteView: View {
+    let show: String
+    @StateObject var viewModel = ViewModel(controller: FetchController())
+
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                Image(.breakingbad)
+                Image(show.lowercased().filter { $0 != " " })
                     .resizable()
                     .frame(width: geometry.size.width * 2.7, height: geometry.size.height)
 
                 VStack {
                     Spacer(minLength: 50)
 
-                    Text("\"You either run from things, or you face them, Mr. White.\"")
-                        .minimumScaleFactor(0.5)
-                        .multilineTextAlignment(.center)
-                        .foregroundStyle(.white)
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .padding()
-                        .background(.black.opacity(0.5))
-                        .clipShape(RoundedRectangle(cornerRadius: 25))
-                        .padding()
+                    switch viewModel.status {
+                    case .notStarted:
+                        EmptyView()
 
-                    ZStack(alignment: .bottom) {
-                        Image(.jessepinkman)
-                            .resizable()
-                            .scaledToFill()
+                    case .fetching:
+                        ProgressView()
 
-                        Text("Jesse Pinkman")
+                    case .success(let data):
+                        Text("\"\(data.quote.quote)\"")
+                            .minimumScaleFactor(0.5)
+                            .multilineTextAlignment(.center)
                             .foregroundStyle(.white)
-                            .padding(10)
-                            .frame(maxWidth: .infinity)
-                            .background(.ultraThinMaterial)
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .padding()
+                            .background(.black.opacity(0.5))
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .padding()
+
+                        ZStack(alignment: .bottom) {
+                            AsyncImage(url: data.character.images[0]) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(
+                                width: geometry.size.width / 1.1,
+                                height: geometry.size.height / 1.8
+                            )
+
+                            Text(data.character.name)
+                                .foregroundStyle(.white)
+                                .padding(10)
+                                .frame(maxWidth: .infinity)
+                                .background(.ultraThinMaterial)
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 20.0))
+                        .frame(
+                            width: geometry.size.width / 1.1,
+                            height: geometry.size.height / 1.8
+                        )
+
+                    case .failed:
+                        EmptyView()
                     }
-                    .clipShape(RoundedRectangle(cornerRadius: 20.0))
-                    .frame(width: geometry.size.width / 1.1, height: geometry.size.height / 1.8)
 
                     Spacer()
 
                     Button {
-
+                        Task {
+                            await viewModel.getData(for: show)
+                        }
                     } label: {
                         Text("Get Random Quote")
                             .font(.title)
@@ -68,6 +96,6 @@ struct QuoteView: View {
 }
 
 #Preview {
-    QuoteView()
+    QuoteView(show: "Breaking Bad")
         .preferredColorScheme(.dark)
 }
